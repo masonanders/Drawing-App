99d384858169ccb003d354f741eed66e32425950
MasonA <mason@ourprairie.net>
2018-12-25 16:43:26 -0600

    add drawing functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
   console.assert(resizeCanvas(canvas), "Could not resize canvas element!");
+  const ctx = canvas.getContext("2d");
+
+  let mouseIsDown = false;
+
+  document.addEventListener("mousedown", e => {
+    mouseIsDown = true;
+    beginDraw(e, ctx);
+  });
+
+  document.addEventListener("mousemove", e => {
+    if (mouseIsDown) executeDraw(e, ctx);
+  });
+
+  document.addEventListener("mouseup", () => {
+    mouseIsDown = false;
+  });
 });
 
 function resizeCanvas(canvasEl) {
@@ -12,4 +28,16 @@ function resizeCanvas(canvasEl) {
   } catch {
     return false;
   }
+}
+
+function beginDraw(event, ctx) {
+  const { clientX, clientY } = event;
+  ctx.beginPath();
+  ctx.moveTo(clientX, clientY);
+}
+
+function executeDraw(event, ctx) {
+  const { clientX, clientY } = event;
+  ctx.lineTo(clientX, clientY);
+  ctx.stroke();
 }
--- bundle.js
+++ bundle.js
@@ -96,6 +96,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("canvas");
   console.assert(resizeCanvas(canvas), "Could not resize canvas element!");
+  const ctx = canvas.getContext("2d");
+
+  let mouseIsDown = false;
+
+  document.addEventListener("mousedown", e => {
+    mouseIsDown = true;
+    beginDraw(e, ctx);
+  });
+
+  document.addEventListener("mousemove", e => {
+    if (mouseIsDown) executeDraw(e, ctx);
+  });
+
+  document.addEventListener("mouseup", () => {
+    mouseIsDown = false;
+  });
 });
 
 function resizeCanvas(canvasEl) {
@@ -107,6 +123,18 @@ function resizeCanvas(canvasEl) {
   } catch {
     return false;
   }
+}
+
+function beginDraw(event, ctx) {
+  const { clientX, clientY } = event;
+  ctx.beginPath();
+  ctx.moveTo(clientX, clientY);
+}
+
+function executeDraw(event, ctx) {
+  const { clientX, clientY } = event;
+  ctx.lineTo(clientX, clientY);
+  ctx.stroke();
 }
 
 
